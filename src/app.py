02058d0bbb9363a9a3a8592b21a40b70294ea0f0
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-07-25 22:43:24 +0100

    add auth_api router

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from src.routers import coins_api, coins_ui
+from src.routers import coins_api, coins_ui, auth_api
 
 app = FastAPI()
 
 app.include_router(coins_api.router)
 app.include_router(coins_ui.router)
+app.include_router(auth_api.router)
