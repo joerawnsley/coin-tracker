0626231111836eb5efc56c7170b7f00f317b4a46
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-07-30 12:39:01 +0100

    delete and remove references to auth_api.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from src.routers import coins_api, coins_ui, auth_api
+from src.routers import coins_api, coins_ui
 
 app = FastAPI()
 
 app.include_router(coins_api.router)
 app.include_router(coins_ui.router)
-app.include_router(auth_api.router)
--- src/routers/auth_api.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter, Form, status
-from fastapi.responses import RedirectResponse
-
-from src.auth import get_user, hash_password, user_db
-
-router = APIRouter()
