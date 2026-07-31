5ab779faa2c8e3e4d0ab7ea41ca5823fea62f946
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-07-31 15:36:08 +0100

    require credentials or token for CRUD routes

--- src/routers/coins_api.py
+++ src/routers/coins_api.py
@@ -6,6 +6,7 @@ from src.input_models import NewCoin, NewDuty, DutyUpdate
 from src.utils import coin_to_dict, duty_to_dict
 from src.auth import user_db, get_user_from_token, authenticate_api_call
 from typing import Annotated
+from peewee import IntegrityError
 
 router = APIRouter()
 security = HTTPBasic()
@@ -28,7 +29,16 @@ def list_coins():
     return coin_list
 
 @router.post("/api/coins", status_code=201, response_class=JSONResponse)
-def add_coin(coin: NewCoin):
+def add_coin(
+        coin: NewCoin, 
+        access_token: str | None = None, 
+        credentials: Annotated[HTTPBasicCredentials | None, Depends(security)] = None
+        ):
+    if access_token:
+        get_user_from_token(access_token)
+    else:
+        authenticate_api_call(credentials.username, credentials.password, user_db)
+        
     Coin.create(
         coin_name=coin.coin_name,
         coin_path=coin.coin_path
@@ -48,21 +58,51 @@ def single_coin(coin_path):
     return coin_to_dict(selected_coin)
 
 @router.delete("/api/coins/{coin_path}")
-def delete_coin(coin_path):
-    Coin.delete().where(Coin.coin_path == coin_path).execute()
-    return "Coin deleted"
+def delete_coin(coin_path, 
+        access_token: str | None = None, 
+        credentials: Annotated[HTTPBasicCredentials | None, Depends(security)] = None
+        ):
+    if access_token:
+        get_user_from_token(access_token)
+    else:
+        authenticate_api_call(credentials.username, credentials.password, user_db)   
+    
+    try:
+        Coin.delete().where(Coin.coin_path == coin_path).execute()
+        return "Coin deleted"
+    except IntegrityError:
+        return "must remove associated duties before deleting coin"
 
-# for adding and removing duties from coins
 @router.put("/api/coins/{coin_path}/add-duties", response_class=JSONResponse)
-def add_duties_to_coin(coin_path, duties: list[int]):
+def add_duties_to_coin(
+        coin_path,
+        duties: list[int], 
+        access_token: str | None = None, 
+        credentials: Annotated[HTTPBasicCredentials | None, Depends(security)] = None
+        ):
+    if access_token:
+        get_user_from_token(access_token)
+    else:
+        authenticate_api_call(credentials.username, credentials.password, user_db)
+        
     selected_coin = Coin.get(Coin.coin_path == coin_path)
     for number in duties:
         selected_coin.duties.add(Duty.get(Duty.duty_number == number))
     return coin_to_dict(selected_coin)
 
-
+# need to authenticate --------------------------------------------------------------
 @router.put("/api/coins/{coin_path}/remove-duties", response_class=JSONResponse)
-def remove_duties_from_coin(coin_path, duties: list[int]):
+def remove_duties_from_coin(
+        coin_path,
+        duties: list[int], 
+        access_token: str | None = None, 
+        credentials: Annotated[HTTPBasicCredentials | None, Depends(security)] = None
+        ):
+    if access_token:
+        get_user_from_token(access_token)
+    else:
+        authenticate_api_call(credentials.username, credentials.password, user_db)
+        
     selected_coin = Coin.get(Coin.coin_path == coin_path)
     for number in duties:
         selected_coin.duties.remove(Duty.get(Duty.duty_number == number))
@@ -83,8 +123,18 @@ def mark_coin_complete(
     updated_coin = Coin.get(Coin.coin_path == coin_path)
     return coin_to_dict(updated_coin)
 
+# need to authenticate --------------------------------------------------------------
 @router.put("/api/coins/{coin_path}/mark-incomplete", response_class=JSONResponse)
-def mark_coin_incomplete(coin_path):
+def mark_coin_incomplete(
+        coin_path, 
+        access_token: str | None = None, 
+        credentials: Annotated[HTTPBasicCredentials | None, Depends(security)] = None
+        ):
+    if access_token:
+        get_user_from_token(access_token)
+    else:
+        authenticate_api_call(credentials.username, credentials.password, user_db)
+        
     Coin.update({Coin.is_complete: False}).where(Coin.coin_path == coin_path).execute()
     updated_coin = Coin.get(Coin.coin_path == coin_path)
     return coin_to_dict(updated_coin)
@@ -101,7 +151,7 @@ def list_coin_duties(coin_path):
 
 @router.get("/api/duties", response_class=JSONResponse)
 def list_duties():
-    query = Duty.select()
+    query = Duty.select().order_by(Duty.duty_number)
     duty_list = []
     for duty in query:
         duty_list.append(duty_to_dict(duty))
