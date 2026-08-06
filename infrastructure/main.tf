0c27b2471062d75250cd7c57ef1617e5071e9f5f
joerawnsley <joe.rawnsley@digital.cabinet-office.gov.uk>
2026-08-06 16:15:10 +0100

    change repo name to use hyphen

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,7 +33,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:joerawnsley/coin_tracker:*"]
+      values   = ["repo:joerawnsley/coin-tracker:*"]
     }
   }
 }
