83ff55e39c0e40cccf31d603778a4bb3bde82370
joerawnsley <joe.rawnsley@digital.cabinet-office.gov.uk>
2026-08-06 12:20:00 +0100

    update auth unit tests for JWT

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,6 @@
+import os
+
+import jwt
 import pytest
 from argon2 import PasswordHasher
 from fastapi import HTTPException
@@ -18,23 +21,21 @@ def test_get_user_returns_correct_data(full_database):
     user_in_db = get_user_from_username("joe")
     ph.verify(user_in_db.hashed_password, "secret")
     assert user_in_db.username == "joe"
+    assert user_in_db.role == "user"
 
 
 def test_get_user_from_token(mocker):
-    # don't think this is testing anything useful, but leaving it in for now
-    #refactor
     mocker.patch(
-        "src.auth.decode_token",
-        return_value=User(
-            username="joe", hashed_password="fakehashedsecret", role="user"
-        ),
+        "jwt.decode",
+        return_value={"sub": "joe"},
     )
     user = get_user_from_token("joe")
     assert user.username == "joe"
+    assert user.role == "user"
 
 
 def test_get_current_user_not_exists(mocker):
-    mocker.patch("src.auth.decode_token", return_value=None)
+    mocker.patch("jwt.decode", return_value=None)
 
     with pytest.raises(HTTPException):
         get_user_from_token("alice")
@@ -42,7 +43,10 @@ def test_get_current_user_not_exists(mocker):
 
 def test_login_converts_form_submission_to_token(full_database):
     response = login(username="joe", password="secret")
-    set_cookie_header = response.headers.get("set-cookie")
+    set_cookie_header = jwt.decode(
+        response.headers.get("set-cookie"),
+        os.getenv("JWT_SECRET"),
+        algorithms=["HS256"],
+    )
     assert response.status_code == 303
-    assert "access_token=" in set_cookie_header
-    assert "Bearer joe" in set_cookie_header
+    assert set_cookie_header.get("sub") == "joe"
