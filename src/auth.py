ec60d1ad1d4208ae3dd62e9e1303147068420323
joerawnsley <joe.rawnsley@digital.cabinet-office.gov.uk>
2026-08-06 11:49:10 +0100

    bring uuid into user data

--- src/auth.py
+++ src/auth.py
@@ -14,6 +14,7 @@ def get_user_dict_from_db():
             "username": user.username,
             "role": user.role,
             "hashed_password": user.hashed_password,
+            "uuid": user.id,
         }
     return user_dictionary
 
