72bd196749bd807ae819f13c4d30fec19de381cf
joerawnsley <joe.rawnsley@digital.cabinet-office.gov.uk>
2026-08-07 09:35:18 +0100

    disable rate limiting in test environment

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
@@ -10,9 +12,10 @@ limiter = Limiter(key_func=get_remote_address, default_limits=["5/10 seconds", "
 
 app = FastAPI()
 
-app.add_middleware(SlowAPIMiddleware)
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+if os.getenv("DB_ENVIRONMENT") == "prod":
+    app.add_middleware(SlowAPIMiddleware)
+    app.state.limiter = limiter
+    app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.include_router(coins_api.router)
 app.include_router(coins_ui.router)
