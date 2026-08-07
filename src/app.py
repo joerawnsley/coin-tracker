25c73181f15b9c12a5220970cd393575ce7fd452
joerawnsley <joe.rawnsley@digital.cabinet-office.gov.uk>
2026-08-07 14:27:40 +0100

    move limiter logic into src/limiter.py

--- src/app.py
+++ src/app.py
@@ -1,23 +1,18 @@
 import os
 
 from fastapi import FastAPI
-from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
-from slowapi.util import get_remote_address
 
+from src.limiter import limiter, too_many_requests_handler
 from src.routers import coins_api, coins_ui
 
-limiter = Limiter(
-    key_func=get_remote_address, default_limits=["5/10 seconds", "20/minute"]
-)
-
 app = FastAPI()
+
+app.include_router(coins_api.router)
+app.include_router(coins_ui.router)
 
 if os.getenv("DB_ENVIRONMENT") == "prod":
     app.add_middleware(SlowAPIMiddleware)
     app.state.limiter = limiter
-    app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
-
-app.include_router(coins_api.router)
-app.include_router(coins_ui.router)
+    app.add_exception_handler(RateLimitExceeded, too_many_requests_handler)
---
+++ src/limiter.py
@@ -0,0 +1,21 @@
+from fastapi import Request, status
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+limiter = Limiter(
+    key_func=get_remote_address, default_limits=["10/10 seconds", "20/minute"]
+)
+
+templates = Jinja2Templates(directory="src/templates")
+
+
+def too_many_requests_handler(request: Request, exc):
+    if request.url.path == "/login":
+        return RedirectResponse(
+            url="/login?error=Too many login attempts",
+            status_code=status.HTTP_303_SEE_OTHER,
+        )
+    else:
+        return templates.TemplateResponse(request=request, name="429.html", status_code=429)
