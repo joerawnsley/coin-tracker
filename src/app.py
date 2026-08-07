412ba581f98493ef28811d3cef409b9933a05758
joerawnsley <joe.rawnsley@digital.cabinet-office.gov.uk>
2026-08-07 09:58:12 +0100

    update workflow permissions

--- src/app.py
+++ src/app.py
@@ -8,7 +8,9 @@ from slowapi.util import get_remote_address
 
 from src.routers import coins_api, coins_ui
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["5/10 seconds", "20/minute"])
+limiter = Limiter(
+    key_func=get_remote_address, default_limits=["5/10 seconds", "20/minute"]
+)
 
 app = FastAPI()
 
