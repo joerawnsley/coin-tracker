95af6b0783b04857ae523e1b72accfe0c9141cbe
joerawnsley <joe.rawnsley@digital.cabinet-office.gov.uk>
2026-08-10 10:41:12 +0100

    begin to implement validation

--- src/input_models.py
+++ src/input_models.py
@@ -1,8 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
+# #validation for the input data models
+''' 
+to do: 
+- allow spaces in coin_name
+- change error message
+- handle 422 error in the frontend
+- test and build out validation on other input models
+'''
 class NewCoin(BaseModel):
-    coin_name: str
+    coin_name: str = Field(
+        min_length=1, 
+        max_length=50, 
+        pattern=r"^[a-zA-Z0-9]+$")
     coin_path: str
     duties: list[int] | None = None
     is_complete: bool | None = None
